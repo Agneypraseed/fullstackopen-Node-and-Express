0ef00a500c332899e028b62e1aa474e5c5aa7b8e
Agney Praseed <agneysince2000@gmail.com>
2023-10-02 22:06:54 +0530

    3.8: Phonebook backend step8

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const app = express();
 const morgan = require("morgan");
 
 app.use(express.json());
-app.use(morgan('tiny'))
+
+morgan.token("postbody", function getBody(req) {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :postbody"
+  )
+);
 
 let phonebook = [
   {
