e69387a53202f0b6eedd7925e67a048008104579
Agney Praseed <agneysince2000@gmail.com>
2023-10-04 19:55:46 +0530

    3.12: Command-line database

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("Please give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const url = `mongodb+srv://fullstack:${password}@cluster0.vdoiowp.mongodb.net/Phonebook?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const PhoneBookSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", PhoneBookSchema);
+
+if (process.argv.length === 3) {
+  console.log("phonebook:");
+  Person.find({}).then((persons) => {
+    persons.map((person) => console.log(person.name + " " + person.number));
+    mongoose.connection.close();
+    process.exit(1);
+  });
+} else {
+  const personName = process.argv[3];
+  const personNumber = process.argv[4];
+
+  const person = new Person({
+    name: personName,
+    number: personNumber,
+  });
+
+  person.save().then((res) => {
+    console.log(`added ${personName} number ${personNumber} to Phonebook`);
+    mongoose.connection.close();
+  });
+}
